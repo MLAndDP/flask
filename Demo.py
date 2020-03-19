9738dbaf22293b5f3d6b602f299710ed6ffc70ae
杨文波 <yangwenbo@middol.com>
2020-03-19 10:45:07 +0800

    ssss

---
+++ Demo.py
@@ -0,0 +1,12 @@
+from registry import DEMOANNO
+
+@DEMOANNO.hellob
+class Demo(object):
+    def __init__(self):
+        pass
+
+    def helloa(self):
+        print("hello2")
+
+if __name__ == '__main__':
+    print("Hello")
---
+++ DemoAnno.py
@@ -0,0 +1,10 @@
+
+
+class DemoAnno(object):
+
+    def __init__(self, name):
+        self.name = name
+
+    def hellob(self, obj):
+        print(obj)
+        print(self.name)
