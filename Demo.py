975e322b09b39070c2f64a207a71faf77cc07d01
杨文波 <yangwenbo@middol.com>
2020-07-10 14:39:36 +0800

    ssss

--- Demo.py
+++ Demo.py
@@ -1,12 +1,50 @@
-from registry import DEMOANNO
+# from registry import DEMOANNO
+#
+# @DEMOANNO.hellob
+# class Demo(object):
+#     def __init__(self):
+#         pass
+#
+#     def helloa(self):
+#         print("hello2")
+#
+# if __name__ == '__main__':
+#     list = []
+#     for i in range (0,5):
+#         str  = f'{i}'
+#         list.append(str)
+#     print(list)
+from PIL import Image
+import matplotlib.pyplot as plt
 
-@DEMOANNO.hellob
-class Demo(object):
-    def __init__(self):
-        pass
-
-    def helloa(self):
-        print("hello2")
+def cut(im, list, vx, vy):
+    dx = vx
+    dy = vy
+    x1 = 0
+    y1 = 0
+    x2 = vx
+    y2 = vy
+    xNum = 1
+    yNum = 1
+    column = []
+    while xNum <= 5:
+        while yNum <= 4:
+            yNum += 1
+            im2 = im.crop((x1, y1, x2, y2))
+            column.append(im2)
+            y1 = y1 + dy
+            y2 = y1 + vy
+        yNum = 1
+        xNum += 1
+        x1 = x1 + dx
+        x2 = x1 + vx
+        y1 = 0
+        y2 = vy
+        list.append(column)
+        column = []
 
 if __name__ == '__main__':
-    print("Hello")
+    im = Image.open("/Users/yangwenbo/Desktop/Image_20200616090411492.jpg")
+    list = []
+    cut(im, list, 1094, 912)
+    print(list)
--- app.py
+++ app.py
@@ -1,63 +1,94 @@
 from flask import Flask
-from flask import request, Response, render_template
-import torch
-from mmdet.apis import inference_detector, init_detector, show_result
-import numpy
+from flask import request, render_template
+# from mmdet.apis import inference_detector, init_detector, show_result
 from PIL import Image
 from gevent import pywsgi
+import gevent
 import io
 import base64
-from flask import jsonify, json
+from flask import json
+from flask_cors import *
+from threading import Condition
+
+class CountDownLatch:
+    def __init__(self, count):
+        self.count = count
+        self.condition = Condition()
+
+    def waiting(self):
+        self.condition.acquire()
+        try:
+            while self.count > 0:
+                self.condition.wait()
+        finally:
+            self.condition.release()
+
+    def count_down(self):
+        self.condition.acquire()
+        self.count -= 1
+        if self.count <= 0:
+            self.condition.notifyAll()
+        self.condition.release()
+
 
 app = Flask(__name__)
-model = init_detector(
-    "/home/yons/zhengxin/mmdetection/configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc0712.py",
-    "/home/yons/zhengxin/mmdetection/faster_rcnn_r50_fpn_1x_voc0712-acc8ede0.pth", device=torch.device('cuda', 0))
-
-
-# model = torch.load()
+CORS(app, resources=r'/*')
 @app.route('/')
 def hello_world():
     # 获取图片文件 name = upload
     return render_template('index.html', name="demo")
 
-
-class Model(object):
-
-    def __init__(self, img, num):
-        self.num = num
-        self.img = img
-
-
-@app.route('/upload/', methods=['POST'])
+@app.route('/upload', methods=['POST'],strict_slashes=False)
 def upload():
     # 获取图片文件 name = upload
     img = request.files['file']
-    score_thr = 0.9
     img = Image.open(img)
-    img = numpy.asarray(img)
-    result = inference_detector(model, img)
-    a = numpy.zeros(shape=(0, 5))
-    result = [result[i] if i == 14 else a for i in range(len(result))]
-    scores = result[14][:, -1]
-    inds = scores > score_thr
-    result[14] = result[14][inds, :]
-    img = show_result(img, result, model.CLASSES, score_thr=score_thr, wait_time=1, show=False)
-    img = Image.fromarray(numpy.uint8(img))
+    list = [[0 for i in range(4)] for j in range(5)]
+    print(list)
+    count = CountDownLatch(20)
+    cutAndDetect(img, list, 1094, 912, count)
+    count.waiting()
+    return json.dumps({'img': list}), 200, {'ContentType': 'application/json'}
+
+def cutAndDetect(im, list, vx, vy, count):
+    dx = vx
+    dy = vy
+    x1 = 0
+    y1 = 0
+    x2 = vx
+    y2 = vy
+    xNum = 0
+    yNum = 0
+    while xNum <= 4:
+        while yNum <= 3:
+            g = gevent.spawn(check, im, x1, y1, x2, y2, list, xNum, yNum, count)
+            g.join()
+            yNum += 1
+            y1 = y1 + dy
+            y2 = y1 + vy
+        yNum = 1
+        xNum += 1
+        x1 = x1 + dx
+        x2 = x1 + vx
+        y1 = 0
+        y2 = vy
+
+def check(im, x1, y1, x2, y2, list, i1, i2, count):
+    im2 = im.crop((x1, y1, x2, y2))
+    # TODO 这里引入目标检测并标注boundingBox
+
+    im2 = im2.transpose(Image.ROTATE_90)
     imgByteArr = io.BytesIO()
-    img.save(imgByteArr, format='JPEG')
+    im2.save(imgByteArr, format='JPEG')
     imgByteArr = imgByteArr.getvalue()
-    # 返回图片
-    # m = Model(base64.b64encode(imgByteArr),len(result[14]))
-    # resp = Response(base64.b64encode(imgByteArr), mimetype="application/text")
-    # return jsonify(
-    #     img=base64.b64encode(imgByteArr),
-    #     num=len(result[14]))
-    print(type(base64.b64encode(imgByteArr)))
-    return json.dumps({'img': str(base64.b64encode(imgByteArr), encoding = "utf-8"), 'num': len(result[14])}), 200, {'ContentType': 'application/json'}
+    list[i1][i2]=str(base64.b64encode(imgByteArr), encoding="utf-8")
+    count.count_down()
 
 
 if __name__ == '__main__':
+
     app.debug = True
-    server = pywsgi.WSGIServer(('0.0.0.0', 5000), app)
+    server = pywsgi.WSGIServer(("0.0.0.0", 5000), app)
     server.serve_forever()
+
+
